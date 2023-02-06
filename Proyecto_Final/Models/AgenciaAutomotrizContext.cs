a524dcbc4474aa56e46689d2a69d20c1eb4e5ddf
perezhernandezjazziel183@gmail.com <perezhernandezjazziel183@gmail.com>
2023-02-06 11:10:48 -0600

    Eliminación del metodo OnConfiguring

--- Proyecto_Final/Models/AgenciaAutomotrizContext.cs
+++ Proyecto_Final/Models/AgenciaAutomotrizContext.cs
@@ -21,9 +21,9 @@ public partial class AgenciaAutomotrizContext : DbContext
 
     public virtual DbSet<Refaccione> Refacciones { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=LAPTOP-IQTO5P3K\\SQLEXPRESS; database=Agencia_automotriz; Integrated Security=True; TrustServerCertificate=True;");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("server=LAPTOP-IQTO5P3K\\SQLEXPRESS; database=Agencia_automotriz; Integrated Security=True; TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
