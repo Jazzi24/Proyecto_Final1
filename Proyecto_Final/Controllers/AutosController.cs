91114a4658e6445d00dc345471fcae16f01806a0
perezhernandezjazziel183@gmail.com <perezhernandezjazziel183@gmail.com>
2023-02-06 11:22:28 -0600

    Creación de la clase autoscontroller

---
+++ Proyecto_Final/Controllers/AutosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Final.Models;
+
+namespace Proyecto_Final.Controllers
+{
+    public class AutosController : Controller
+    {
+        private readonly AgenciaAutomotrizContext _context;
+
+        public AutosController(AgenciaAutomotrizContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Autos
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Autos.ToListAsync());
+        }
+
+        // GET: Autos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Autos == null)
+            {
+                return NotFound();
+            }
+
+            var auto = await _context.Autos
+                .FirstOrDefaultAsync(m => m.IdAuto == id);
+            if (auto == null)
+            {
+                return NotFound();
+            }
+
+            return View(auto);
+        }
+
+        // GET: Autos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Autos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdAuto,Modelo,Color,Precio,Cantidad")] Auto auto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(auto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(auto);
+        }
+
+        // GET: Autos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Autos == null)
+            {
+                return NotFound();
+            }
+
+            var auto = await _context.Autos.FindAsync(id);
+            if (auto == null)
+            {
+                return NotFound();
+            }
+            return View(auto);
+        }
+
+        // POST: Autos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdAuto,Modelo,Color,Precio,Cantidad")] Auto auto)
+        {
+            if (id != auto.IdAuto)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(auto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AutoExists(auto.IdAuto))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(auto);
+        }
+
+        // GET: Autos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Autos == null)
+            {
+                return NotFound();
+            }
+
+            var auto = await _context.Autos
+                .FirstOrDefaultAsync(m => m.IdAuto == id);
+            if (auto == null)
+            {
+                return NotFound();
+            }
+
+            return View(auto);
+        }
+
+        // POST: Autos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Autos == null)
+            {
+                return Problem("Entity set 'AgenciaAutomotrizContext.Autos'  is null.");
+            }
+            var auto = await _context.Autos.FindAsync(id);
+            if (auto != null)
+            {
+                _context.Autos.Remove(auto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AutoExists(int id)
+        {
+          return _context.Autos.Any(e => e.IdAuto == id);
+        }
+    }
+}
